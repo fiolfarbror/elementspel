68d6a2a17d70a8bd209757c22f3bb588f9874d0a
fiolfarbror <wilper@gmail.com>
2015-10-18 14:30:52 +0200

    A simple game board is shown.

--- src/game.py
+++ src/game.py
@@ -8,6 +8,22 @@ if not pygame.font: print 'Warning, fonts disabled'
 if not pygame.mixer: print 'Warning, sound disabled'
 
 
+def draw_board(boardSurface, boardColors):
+    """
+    Draws the game board onto a surface.
+    The board position 0,0 is the upper left corner.
+    The screen position 0,0 is also the upper left corner.
+    Increasing X coordinate moves to the right.
+    Increasing Y coordinate moves to the bottom. 
+    """
+    boardSurface.fill((250,250,250))
+    for y in range(0,11):
+        for x in range(0,11):
+            boardSurface.fill(boardColors[x][y], # Color
+                              Rect(x*40+1, y*40+1, 38, 38)) # Rect upper left 
+                                                            # corner and size 
+
+
 def main():
     """this function is called when the program starts.
        it initializes everything it needs, then runs in
@@ -18,34 +34,60 @@ def main():
     pygame.display.set_caption('Elementspel')
     pygame.mouse.set_visible(1)
 
+    boardsize = 11
+    squaresize = 40
+#boardColors is a 2-dimensional array that contains which color each
+#square of the board should have.
+    boardColors = [[0 for j in range(boardsize)] for i in range(boardsize)]    
+    for y in range(0,11):
+        for x in range(0,11):
+            #This is a bit crude. I use the RGB values directly.
+            #We probably don't want it to work like that later.
+            #For now there's a color shift across the board, we
+            #will remove it once you understand how the board indexing
+            #works.
+            boardColors[x][y] = (30+x*10,30,30)
+
+#The picture of the game board itself
+    boardSurface = pygame.Surface((boardsize*squaresize, boardsize*squaresize))
+    boardSurface = boardSurface.convert()
+    boardSurface.fill((0,0,0))
+    draw_board(boardSurface, boardColors)
+
 #Create The Backgound
     background = pygame.Surface(screen.get_size())
     background = background.convert()
     background.fill((250, 150, 150))
+
 #Display The Background
+#This is only the first time, it will be redrawn in the Main Loop below.
     screen.blit(background, (0, 0))
+    screen.blit(boardSurface,(10,10))
     pygame.display.flip()
 
 
     clock = pygame.time.Clock()
 
 
-    i = 0
 #Main Loop
     while 1:
         clock.tick(60)
-        i = i + 1
-        print "Hej, kaka" + str(i)
-    #Handle Input Events
+
+        #Handle Input Events
         for event in pygame.event.get():
             if event.type == QUIT:
                 return
             elif event.type == KEYDOWN and event.key == K_ESCAPE:
                 return
 
+        #Draw a new board
+        draw_board(boardSurface, boardColors)
 
-    #Draw Everything
+        #Paste the drawing of the background and the board on the screen
         screen.blit(background, (0, 0))
+        screen.blit(boardSurface,(10,10))
+        
+        #Show the screen to the player
         pygame.display.flip()
 
 
